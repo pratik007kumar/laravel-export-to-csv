c20dcc3c2e15a481fe40f6f488bc5fa60c56a63c
Pratik kumar <pratik007kumar@users.noreply.github.com>
2019-07-08 15:26:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ public function get_export()
         $table = StudentModel::all();
         $file = fopen('file.csv', 'w');
         foreach ($table as $row) {
-            fputcsv($file, $row->to_array());
+            fputcsv($file, $row->toArray());
         }
         fclose($file);
         return Redirect::to('/');
